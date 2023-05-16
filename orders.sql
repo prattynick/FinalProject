c9fab2b7407466a6d3f11ea0b868964379ec9d79
prattynick <nptoulou@buffalo.edu>
2023-05-16 18:48:16 -0400

    Fourth Commit

--- orders.sql
+++ orders.sql
@@ -23,10 +23,9 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
-  `ID` int NOT NULL,
-  `NAME` text NOT NULL,
-  `PRICE` text NOT NULL,
-  PRIMARY KEY (`ID`)
+  `ID` int DEFAULT NULL,
+  `NAME` varchar(255) DEFAULT NULL,
+  `PRICE` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -48,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-16 18:19:36
+-- Dump completed on 2023-05-16 18:47:48
