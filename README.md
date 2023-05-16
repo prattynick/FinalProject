617f822a8bea9b6c914b2d63b4c634d36f6cd173
prattynick <nptoulou@buffalo.edu>
2023-05-16 18:04:47 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FinalProject
+
+For this project I decided to make a web app for a fictional restaurant titled Los Pollos Hermanos. 
+In this web app I have a page for the menu, a page to place orders, and a place to view orders.
+The demo video will show the web app working.
---
+++ manage.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/menu')
+def menu():
+	conn = sqlite3.connect('restaurant.db')
+	c = conn.cursor()
+	c.execute('SELECT ID, NAME, PRICE FROM menu')
+	items = c.fetchall()
+	return render_template('menu.html', items=items)
+
+@app.route('/order', methods=['GET', 'POST'])
+def order():
+	if request.method == 'POST':
+		ID = request.form['ID']
+		NAME = request.form['NAME']
+		PRICE = request.form['PRICE']
+
+		conn = sqlite3.connect('orders.db')
+		c = conn.cursor()
+		c.execute('''INSERT INTO orders (ID, NAME, PRICE) VALUES (?, ?, ?)''', (ID, NAME, PRICE))
+		conn.commit()
+		return 'Order Successful'
+		conn.close()
+	else:
+		return render_template('order.html')
+		conn.close()
+
+@app.route('/orders')
+def orders():
+	conn = sqlite3.connect('orders.db')
+	c = conn.cursor()
+	c.execute('SELECT * FROM orders')
+	orders = c.fetchall()
+	conn.close()
+	return render_template('orders.html', orders=orders)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
